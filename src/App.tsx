30f6e78a4c58f497bd043c6442207390f2547c9b
wiki-games <wikigames420@gmail.com>
2023-09-25 07:59:20 -0700

    changed color scheme for hopefully the last time, removed logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Analytics } from '@vercel/analytics/react';
 
@@ -10,24 +8,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Wiki-Games ~ Under Construction</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          yes
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        testtext
       </p>
       <body>
         <Analytics />
