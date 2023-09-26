89748a31266d6e5cfb61c758522390b7f0e5d4fb
wiki-games <wikigames420@gmail.com>
2023-09-25 17:54:41 -0700

    fixed colors not working on dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { Analytics } from '@vercel/analytics/react';
+import { useState } from "react";
+import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-      </div>
+      <div></div>
       <h1>Wiki-Games ~ Under Construction</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          yes
-        </p>
+        <p>yes</p>
       </div>
-      <p className="read-the-docs">
-        testtext
-      </p>
+      <p className="read-the-docs">testtext</p>
       <body>
         <Analytics />
       </body>
     </>
-  )
+  );
 }
-
-export default App
