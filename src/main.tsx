9220dd0eaed522047c9ddb82ad0fde730472d934
wiki-games <wikigames420@gmail.com>
2023-10-08 12:10:55 -0700

    constructed a lil bit of backend

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,27 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+/*
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+*/
