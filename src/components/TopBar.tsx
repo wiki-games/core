06fb913607fab8f3fbc846b0330ccaa929b9f006
wiki-games <wikigames420@gmail.com>
2023-11-12 19:18:05 -0800

    got rid of errors in wikimark due to unused params

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import "./TopBar.css";
 export default function TopBar() {
   return (
     <div className="topbar">
-      <a href="/gameName/Stray">Wiki-Games</a>
+      <a href="/gameName/Stray">Wiki-Games - In Development</a>
     </div>
   );
 }
